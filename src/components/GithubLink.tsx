ca69840f8b9a7ded1eff8386419e1fa5833930e7
Samuel Newman <mozzius@protonmail.com>
2025-09-08 17:31:16 +0300

    add og image

--- src/components/GithubLink.tsx
+++ src/components/GithubLink.tsx
@@ -1,12 +1,12 @@
 function GithubLink() {
   return (
-    <p className="text-gray-400 mt-2">
+    <p className="text-gray-400 my-auto mr-2 hover:underline">
       <a
         href="https://github.com/rafaeleyng/pin2saved"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Code on Github
+        <span className="hidden min-sm:inline">Code on </span>GitHub
       </a>
     </p>
   );
