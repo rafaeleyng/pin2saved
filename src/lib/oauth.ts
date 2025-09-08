9c95fa517d54db76a037759ee15adf28032a0ce5
Samuel Newman <mozzius@protonmail.com>
2025-09-08 12:11:21 +0300

    rename app in client metadata

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -4,15 +4,15 @@ export const client = new BrowserOAuthClient({
   handleResolver: "https://bsky.social",
   clientMetadata: {
     client_id: import.meta.env.PROD
-      ? `https://bsky-prefs.vercel.app/oauth-client-metadata.json`
+      ? `https://pin2saved.vercel.app/oauth-client-metadata.json`
       : `http://localhost?redirect_uri=${encodeURIComponent(`http://127.0.0.1:3000/oauth/callback`)}&scope=${encodeURIComponent("atproto transition:generic")}`,
     redirect_uris: [
       import.meta.env.PROD
-        ? `https://bsky-prefs.vercel.app/oauth/callback`
+        ? `https://pin2saved.vercel.app/oauth/callback`
         : `http://127.0.0.1:3000/oauth/callback`,
     ],
-    client_name: "Bluesky preferences helper",
-    client_uri: "https://bsky-prefs.vercel.app",
+    client_name: "pin2saved",
+    client_uri: "https://pin2saved.vercel.app",
     scope: "atproto transition:generic",
     grant_types: ["authorization_code", "refresh_token"],
     response_types: ["code"],
